fc4d1511c361196683f0ddc2e6398dc6233a58a9
danieljj28 <danijj28@gmail.com>
2024-05-10 11:18:28 +0200

    Finalizada la aplicación. Más adelante se introduciran cambios

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 3. Instalar json-server
 4. Levantar backend ```npm run backend```
 5. Ejecutar la app ```npm start``` o bien ```npm serve -0```
+6. En la pantalla de login, no hace falta introducir nada. Se logea con un usuario por defecto.
--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { HeroService } from '../../services/hero.service';
 import { FormControl } from '@angular/forms';
-import { Hero } from '../../interfaces/hero.interface';
+
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+import { HeroService } from '../../services/hero.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-search-page',
@@ -24,6 +26,7 @@ export class SearchPageComponent {
       .subscribe( heroes => this.heroes = heroes);
   }
 
+
   onSelectedOption( event: MatAutocompleteSelectedEvent ): void {
     if( !event.option.value ) {
       this.selectedHero = undefined;
@@ -34,6 +37,8 @@ export class SearchPageComponent {
     this.searchInput.setValue( hero.superhero );
 
     this.selectedHero = hero;
+
   }
+
 
 }
